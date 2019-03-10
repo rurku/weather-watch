23646c84f36a200450f9ad71f19335f48d2c7754
Jacek Olearczyk <rurku@rurku.com>
2019-03-10 14:30:12 +0100

    Added time input, prev/next period buttons, better date format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,41 @@ import * as Chart from 'chart.js';
 import { groupBy as _groupBy, map as _map, minBy as _minBy, maxBy as _maxBy, meanBy as _meanBy } from 'lodash-es';
 
 import * as AWSConfig from './aws-config.json';
+import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { FormControl } from '@angular/forms';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'YYYY-MM-DD',
+  },
+  display: {
+    dateInput: 'YYYY-MM-DD',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+    // application's root module. We provide it at the component level here, due to limitations of
+    // our example generation script.
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ],
 })
 export class AppComponent implements OnInit {
   chart: Chart;
   private dynamodb: DynamoDB;
   reading: object;
   public date: moment.Moment;
+  public time: string;
   public period: string;
   private refreshCount = 0;
 
@@ -29,7 +53,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.date = moment();
+    this.setDate(moment());
     this.period = '1 day';
 
     this.scheduleRefresh();
@@ -40,11 +64,44 @@ export class AppComponent implements OnInit {
     this.scheduleRefresh();
   }
 
+  private addPeriod(t: moment.Moment, subtract = false): moment.Moment {
+    const regex = /^(?<number>\d+) *(?<period>\w+)$/;
+    const match = regex.exec(this.period);
+    const clone = moment(t);
+    if (match) {
+      const period = match.groups.period as moment.unitOfTime.DurationConstructor;
+      clone.add((subtract ? -1 : 1) * parseInt(match.groups.number, 10), period);
+    }
+    return clone;
+  }
+
+  nextPeriod() {
+    const dateTime = this.addPeriod(this.getDate());
+    this.setDate(dateTime);
+    this.scheduleRefresh();
+  }
+
+  previousPeriod() {
+    const dateTime = this.addPeriod(this.getDate(), true);
+    this.setDate(dateTime);
+    this.scheduleRefresh();
+  }
+
+
+  setDate(date: moment.Moment) {
+    this.date = moment(date).startOf('day');
+    this.time = date.format('HH:mm');
+  }
+
   scheduleRefresh() {
     this.refreshCount ++;
     setTimeout((refreshCount) => {
       this.refresh(refreshCount);
     }, 0, this.refreshCount);
+  }
+
+  private getDate(): moment.Moment {
+    return moment(this.date).add(moment.duration(this.time));
   }
 
   async refresh(refreshCount: number) {
@@ -67,63 +124,57 @@ export class AppComponent implements OnInit {
     const lastTimestamp = moment.unix(parseInt(lastReading.Timestamp.N, 10));
     this.reading = {temp: parseFloat(lastReading.Temp.N).toFixed(1), time: lastTimestamp.format('YYYY-MM-DD HH:mm:ss')};
 
-    const regex = /^(?<number>\d+) *(?<period>\w+)$/;
-    const match = regex.exec(this.period);
-    if (match) {
-      const rangeEnd = moment(this.date).utc().unix();
-      const period = match.groups.period as moment.unitOfTime.DurationConstructor;
-      const rangeStart = moment(this.date).utc().add(-parseInt(match.groups.number, 10), period).unix();
-      const resolution = 672;
+    const rangeEnd = this.getDate().utc().unix();
+    const rangeStart = this.addPeriod(this.getDate(), true).utc().unix();
+    const resolution = 672;
 
-      const minSampleRate = (rangeEnd - rangeStart) / resolution;
+    const minSampleRate = (rangeEnd - rangeStart) / resolution;
 
-      let formatPk: ((t: moment.Moment) => string);
-      let pkName: string;
-      let pkUnit: string;
-      let indexName: string;
-      if (minSampleRate >= 3 * 60 * 60) {
-        pkName = 'YearKey';
-        pkUnit = 'year';
-        formatPk = (t) => moment(t).utc().format('YYYY');
-        indexName = 'ByYear';
-      } else if (minSampleRate >= 15 * 60) {
-        pkName = 'MonthKey';
-        pkUnit = 'month';
-        formatPk = (t) => moment(t).utc().format('YYYY-MM');
-        indexName = 'ByMonth';
-      } else {
-        pkName = 'DayKey';
-        pkUnit = 'day';
-        formatPk = (t) => moment(t).utc().format('YYYY-MM-DD');
-        indexName = undefined;
-      }
-
-      const items: DynamoDB.AttributeMap[] = [];
-
-      const pkRange = moment.unix(rangeStart).utc();
-      const pkRangeEnd = formatPk(moment.unix(rangeEnd).utc());
-      while (formatPk(pkRange) <= pkRangeEnd) {
-        const params: DynamoDB.QueryInput = {
-          TableName: 'temperature',
-          KeyConditionExpression: `${pkName} = :pk AND #ts BETWEEN :from AND :to`,
-          ExpressionAttributeValues: {
-            ':from': {N: rangeStart.toFixed()},
-            ':to': {N: rangeEnd.toFixed()},
-            ':pk': {S: formatPk(pkRange)}
-          },
-          ExpressionAttributeNames: {'#ts': 'Timestamp', '#temp': 'Temp'},
-          ProjectionExpression: '#ts, #temp',
-          IndexName: indexName,
-        };
-        const result = await this.dynamodb.query(params).promise();
-        if (refreshCount !== this.refreshCount) { return; }
-        items.push(...result.Items);
-        pkRange.add(1, pkUnit as moment.unitOfTime.DurationConstructor);
-      }
-
-      this.showChart(items, rangeStart, rangeEnd, resolution);
+    let formatPk: ((t: moment.Moment) => string);
+    let pkName: string;
+    let pkUnit: string;
+    let indexName: string;
+    if (minSampleRate >= 3 * 60 * 60) {
+      pkName = 'YearKey';
+      pkUnit = 'year';
+      formatPk = (t) => moment(t).utc().format('YYYY');
+      indexName = 'ByYear';
+    } else if (minSampleRate >= 15 * 60) {
+      pkName = 'MonthKey';
+      pkUnit = 'month';
+      formatPk = (t) => moment(t).utc().format('YYYY-MM');
+      indexName = 'ByMonth';
+    } else {
+      pkName = 'DayKey';
+      pkUnit = 'day';
+      formatPk = (t) => moment(t).utc().format('YYYY-MM-DD');
+      indexName = undefined;
     }
 
+    const items: DynamoDB.AttributeMap[] = [];
+
+    const pkRange = moment.unix(rangeStart).utc();
+    const pkRangeEnd = formatPk(moment.unix(rangeEnd).utc());
+    while (formatPk(pkRange) <= pkRangeEnd) {
+      const params: DynamoDB.QueryInput = {
+        TableName: 'temperature',
+        KeyConditionExpression: `${pkName} = :pk AND #ts BETWEEN :from AND :to`,
+        ExpressionAttributeValues: {
+          ':from': {N: rangeStart.toFixed()},
+          ':to': {N: rangeEnd.toFixed()},
+          ':pk': {S: formatPk(pkRange)}
+        },
+        ExpressionAttributeNames: {'#ts': 'Timestamp', '#temp': 'Temp'},
+        ProjectionExpression: '#ts, #temp',
+        IndexName: indexName,
+      };
+      const result = await this.dynamodb.query(params).promise();
+      if (refreshCount !== this.refreshCount) { return; }
+      items.push(...result.Items);
+      pkRange.add(1, pkUnit as moment.unitOfTime.DurationConstructor);
+    }
+
+    this.showChart(items, rangeStart, rangeEnd, resolution);
   }
 
   private showChart(samples: DynamoDB.AttributeMap[], timestampFrom: number, timestampTo: number, resolution: number) {
@@ -142,15 +193,6 @@ export class AppComponent implements OnInit {
         x: key,
       };
     });
-
-    const minValue = _minBy(average, x => x.y).y;
-    const maxValue = _maxBy(average, x => x.y).y;
-    let suggestedLow: number;
-    let suggestedHigh: number;
-    if (maxValue - minValue < 6) {
-      suggestedLow = (maxValue + minValue) / 2 + 3;
-      suggestedHigh = (maxValue + minValue) / 2 - 3;
-    }
 
     if (!this.chart) {
       this.chart = new Chart('canvas2', {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {MatDatepickerModule, MatInputModule, MatButtonToggleModule} from '@angular/material';
+import {MatDatepickerModule, MatInputModule, MatButtonToggleModule, MatButtonModule} from '@angular/material';
 import {MatMomentDateModule} from '@angular/material-moment-adapter';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, MatDatepickerModule, MatMomentDateModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatButtonToggleModule
+    BrowserModule, MatDatepickerModule, MatMomentDateModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatButtonToggleModule, MatIconModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
